a9b3088acba07876497abe4e17f786b4f30932ad
twinh <twinhuang@qq.com>
2019-03-04 13:15:19 +0800

    refactoring: .list-feedback > .list-arrow

--- resources/views/ssoUserMobile/menu.php
+++ resources/views/ssoUserMobile/menu.php
@@ -1,6 +1,6 @@
 <ul class="list list-indented">
   <li class="list-item-link">
-    <a class="list-item list-has-feedback" href="<?= $url('sso-user-mobile') ?>">
+    <a class="list-item list-has-arrow" href="<?= $url('sso-user-mobile') ?>">
       <div class="list-col list-col-icon">
         <i class="list-icon ni ni-gift"></i>
       </div>
@@ -9,7 +9,7 @@
           绑定手机
         </h4>
       </div>
-      <i class="bm-angle-right list-feedback"></i>
+      <i class="bm-angle-right list-arrow"></i>
     </a>
   </li>
 </ul>
